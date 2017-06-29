2a845621a64efab8dfc7d6a5017177e5eccc6266
Carson Haskell <carson.haskell@gmail.com>
2017-06-29 09:38:37 -0700

    update js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,5 @@ var add = function(num1, num2) {
 
 var num1 = parseInt(prompt("Enter a number:"));
 var num2 = parseInt(prompt("Enter another number:"));
-
-alert(add(num1, num2));
+var result = add(num1, num2);
+alert(result);
