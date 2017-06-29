24f77e50c269edbe12c6cc60ecb4e486f9a7ec98
Carson Haskell <carson.haskell@gmail.com>
2017-06-29 10:18:23 -0700

    nothing important

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var add = function(num1, num2) {
   return num1 + num2;
 }
+var multiply = function(num1, num2) {
+  return num1 * num2;
+}
+var subtract = function(num1, num2) {
+  return num1 - num2;
+}
+var divide = function(num1, num2) {
+  return num1 / num2;
+}
 
 var num1 = parseInt(prompt("Enter a number:"));
 var num2 = parseInt(prompt("Enter another number:"));
-var result = add(num1, num2);
+var result = subtract(num1, num2);
 alert(result);
